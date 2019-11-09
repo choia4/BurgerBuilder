151a0db6a96b6dce69b142605241dd12a9c3a8bb
choia4 <49245467+choia4@users.noreply.github.com>
2019-11-08 17:07:06 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
+import Orders from './containers/Orders/Orders'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //switch chooses one route to go to
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
